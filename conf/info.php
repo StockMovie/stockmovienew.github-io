3c2b8bcafef325b9391267c50ca79e2c9d7a10cc
StockMovie <154013958+StockMovie@users.noreply.github.com>
2023-12-17 01:04:40 +0700

    Add files via upload

---
+++ conf/info.php
@@ -0,0 +1,13 @@
+<?php
+
+$apikey = "6b1147e0fcf05105de70f305f41e3fd2";
+
+$sitename = "Update Film";
+$tagline = "Info Film Update";
+$email = "hellojuniordev@gmail.com";
+$url = "http://juniordev.net";
+
+$imgurl_1 = "http://image.tmdb.org/t/p/w500";
+$imgurl_2 = "http://image.tmdb.org/t/p/w300";
+
+?>
